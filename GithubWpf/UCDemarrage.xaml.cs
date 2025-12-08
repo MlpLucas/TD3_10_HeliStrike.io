9904b96f307d099ca0a5c7b8d07534eb6b32067c
MlpLucas <Lucas.Melampe@etu.univ-smb.fr>
2025-12-08 15:48:39 +0100

    -DEBUT ECRAN DEMARRAGE -RECTIFICATION FENETRE

--- GithubWpf/UCDemarrage.xaml.cs
+++ GithubWpf/UCDemarrage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace GithubWpf
@@ -17,7 +18,7 @@ namespace GithubWpf
     /// <summary>
     /// Logique d'interaction pour UCDemarrage.xaml
     /// </summary>
-    public partial class UCDemarrage : Window
+    public partial class UCDemarrage : UserControl
     {
         public UCDemarrage()
         {
