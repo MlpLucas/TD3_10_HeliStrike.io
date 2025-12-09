4fcc4854398e8cc7a12fdf2bcd86d72aeeb4a8b5
ArnachellumAdrien <adrien.arnachellum@etu.univ-smb.fr>
2025-12-09 11:23:04 +0100

    Fond qui défile OK

--- GithubWpf/MainWindow.xaml.cs
+++ GithubWpf/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace GithubWpf
 {
@@ -16,9 +17,37 @@ namespace GithubWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static DispatcherTimer minuterie;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            minuterie = new DispatcherTimer();
+            // configure l'intervalle du Timer
+            minuterie.Interval = TimeSpan.FromMilliseconds(16);
+            // associe l’appel de la méthode Jeu à la fin de la minuterie
+            minuterie.Tick += Jeu;
+            // lancement du timer
+            minuterie.Start();
+        }
+
+        private static int pasFond = 4;
+        private void Jeu(object? sender, EventArgs e)
+        {
+            Deplace(imgFond1, pasFond);
+            Deplace(imgFond2, pasFond);
+        }
+
+        public void Deplace(Image image, int pas)
+        {
+            Canvas.SetTop(image, Canvas.GetTop(image) + pas);
+            if (Canvas.GetTop(image) >= image.Height)
+                Canvas.SetTop(image, -image.ActualHeight + pas);
         }
     }
 }
